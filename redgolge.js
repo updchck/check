fc46c8fe450d94cc904b58b5de788e6b0bef6112
aliredpr <159641269+aliredpr@users.noreply.github.com>
2024-08-12 14:09:55 +0300

    Update

---
+++ redgolge.js
@@ -0,0 +1,10 @@
+{
+    "apiUrl": "https://t.me/reddomvip",
+    "updateConfig": {
+        "updateVersiyon": "1.3.8",
+        "updateTitle": "Şimdi Güncelleme Zamanı!",
+        "updateYenilik": "Medya player için bir kaç uzantı desteği sağlandı, Bazı bölümlerin stabil çalışması için iyileştirmeler yapıldı.....",
+        "updateLink": "https://github.com/redvip.apk"
+    },
+     "appSignature": "9275CEE1FD47B543412DE852C118A0663E40AA83EG12"
+}
